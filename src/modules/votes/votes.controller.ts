706cd1a67e7194a54300b71898c0d1b8a0beb870
perezxalan <perezxalan@gmail.com>
2023-04-26 10:55:10 -0500

    vote system rough

--- src/modules/votes/votes.controller.ts
+++ src/modules/votes/votes.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { VotesService } from './votes.service';
+import { Vote } from './interfaces/votes.interface';
 
 @Controller('votes')
-export class VotesController {}
+export class VotesController {
+    constructor(private readonly votesService: VotesService) {}
+
+  // upvote
+    @Post('upvote/:userId/:videoId')
+  async upvote(
+    @Param('userId') userId: string,
+    @Param('videoId') videoId: string,
+  ): Promise<Vote> {
+      return await this.votesService.upvote(userId, videoId);
+    }
+
+  //  downvote
+    @Post('downvote/:userId/:videoId')
+  async downvote(
+    @Param('userId') userId: string,
+    @Param('videoId') videoId: string,
+  ): Promise<Vote> {
+    return await this.votesService.downvote(userId, videoId);
+    }
+  
+    // total votes by the video ID
+    @Get('total/:videoId')
+    async getTotalVotes(@Param('videoId') videoId: string): Promise<number> {
+    return await this.votesService.getTotalVotes(videoId);
+    }
+}
